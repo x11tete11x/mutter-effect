125a92c5b6510617c015b0c057fec828ba5a9c4e
Tete Plaza <11tete11@gmail.com>
2014-10-16 15:14:51 -0300

    initial files for Mutter Effect

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 mutter-effect
 =============
 
-This is an effect for Kwin, that simulate de open/close window animation of mutter
+This is an effect for Kwin, that simulate de open/close window animation of mutter.
+
+
+It's a fork of "Cool effect" with few patches.
---
+++ contents/code/main.js
@@ -0,0 +1,54 @@
+/*global effect, effects, animate, animationTime, Effect, QEasingCurve*/
+var MutterEffect = {
+  duration: animationTime(150),
+  isfadeWindow: function(window) {
+    if (window.deleted && effect.isGrabbed(window, Effect.WindowClosedGrabRole))
+    {
+      return false;
+    } else if (!window.deleted && effect.isGrabbed(window, Effect.WindowAddedGrabRole))
+    {
+      return false;
+    } else if (window.windowClass.match("plasma|Plasma"))
+    {
+      return false;
+    }
+    return window.onCurrentDesktop && !MutterEffect.isLoginWindow(window) && ! window.desktopWindow &&
+    !window.utility && !window.minimized;
+  },
+  isLoginWindow: function(window) {
+    return window.windowClass == "ksplashx ksplashx" ||
+	   window.windowClass == "ksplashsimple ksplashsimple" ||
+	   window.windowClass == "qt-subapplication ksplashqml";
+  },
+  fadeWindow: function (window, direction) {
+        animate({
+            window: window,
+            duration: MutterEffect.duration,
+            animations: [{
+                type: Effect.Opacity,
+                curve: QEasingCurve.InOutQuad,
+                from: direction === 'in' ? 0.0 : 1.0,
+                to: direction === 'in' ? 1.0 : 0.0
+            }, {
+                type: Effect.Scale,
+                curve: QEasingCurve.InOutQuad,
+                from: direction === 'in' ? 0.75 : 1.0,
+                to: direction === 'in' ? 1.0 : 0.75
+            }]
+        });
+    },
+  shown: function(window) {
+    if (MutterEffect.isfadeWindow(window))
+    MutterEffect.fadeWindow(window, 'in');
+  },
+  closed: function (window) {
+    if (MutterEffect.isfadeWindow(window))
+    MutterEffect.fadeWindow(window, 'out');
+  },
+  init: function () {
+        effects.windowAdded.connect(MutterEffect.shown);
+	effects.windowClosed.connect(MutterEffect.closed);
+  }
+};
+
+MutterEffect.init();
